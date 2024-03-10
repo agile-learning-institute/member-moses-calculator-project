35162d2a6d405679ea08805adcfd6a654c3968c2
wodpachua <molarag21996@gmail.com>
2024-03-11 01:03:36 +0300

    Add functions for basic add, subtract, divide, multiply and percentage operations

---
+++ script.js
@@ -0,0 +1,22 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function percentage(a) {
+  return a / 100;
+}
+
+// console.log(divide(1, 3));
+// console.log(percentage(5));
