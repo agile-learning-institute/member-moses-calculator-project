f855a0532b17e953205bcbfaf602fe86e34368a9
wodpachua <molarag21996@gmail.com>
2024-03-11 04:10:52 +0300

    Add event listeners to number buttons and function to update display on click

--- script.js
+++ script.js
@@ -37,3 +37,38 @@ function operate(operand1, operator, operand2) {
     return percentage(operand1);
   }
 }
+
+let operationDisplay = document.querySelector(".operation-display");
+let resultDisplay = document.querySelector(".result-display");
+
+// const oneBtn = document.querySelector(".btn-1");
+// const twoBtn = document.querySelector(".btn-2");
+// const threeBtn = document.querySelector(".btn-3");
+// const fourBtn = document.querySelector(".btn-4");
+// const fiveBtn = document.querySelector(".btn-5");
+// const sixBtn = document.querySelector(".btn-6");
+// const sevenBtn = document.querySelector(".btn-7");
+// const eightBtn = document.querySelector(".btn-8");
+// const nineBtn = document.querySelector(".btn-9");
+// const zeroBtn = document.querySelector(".btn-0");
+let numberButtons = document.querySelectorAll(".num-btn");
+numberButtons.forEach((button) => {
+  button.addEventListener("click", updateOperationDisplay);
+});
+
+// Buttons Event listeners
+// oneBtn.addEventListener("click", updateOperationDisplay);
+
+// Updates Operations Display
+function updateOperationDisplay(e) {
+  if (operationDisplay.textContent.length < 13) {
+    operationDisplay.textContent += e.target.textContent;
+  } else {
+    operationDisplay.textContent += "";
+  }
+}
+
+// Updates Results Display
+// function updateResultDisplay() {}
+
+// console.log(operationDisplay);
